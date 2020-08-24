80b526a655b07dd5732eb35a7d337eb765c8235d
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-08-24 20:53:12 +0530

    Deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web : gunicorn openapi.wsgi
--- openapi/settings.py
+++ openapi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'eki3256!%v9sagqcvg(g8hn33p4oa$dn=*xw^pix-a*@y_zpi('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
